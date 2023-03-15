039a229ab48989f5140b8253acde66f3c9f6ced7
abil0321 <salsabilmuhammad45@gmail.com>
2023-03-15 18:18:41 +0700

    first commit in week4

---
+++ week4/script.js
@@ -0,0 +1,20 @@
+// import ReactDOM from 'react-dom';
+
+function Hello() {
+  return (
+    <div>
+      <h2>Hello bang React</h2>
+      <p>Saya Frontend Engineer</p>
+    </div>
+  );
+}
+
+
+// const root = document.getElementById("root");
+// ReactDOM.render(<Hello />, root);
+
+// const root = document.getElementById("root");
+// ReactDOM.render(<Hello />, root);
+
+const root = document.getElementById("root");
+ReactDOM.render(<Hello />, root);
