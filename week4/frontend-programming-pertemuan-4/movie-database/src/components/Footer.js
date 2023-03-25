18aaca4c3137b8f3388d6c6caa2d94924d9a1a78
abil0321 <74514963+abil0321@users.noreply.github.com>
2023-03-25 10:26:23 +0700

    Delete week4 directory

--- week4/frontend-programming-pertemuan-4/movie-database/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-    return (
-      <footer>
-        <h2>Copyright @MuhammadSalsabil45</h2>
-        {/* <p>Created by React.js</p> */}
-      </footer>
-    );
-  }
-export default Footer;
