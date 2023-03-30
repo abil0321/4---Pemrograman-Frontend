f4e905241c2b0c48c237ed6c894fe044c42c0075
abil0321 <salsabilmuhammad45@gmail.com>
2023-03-30 08:16:09 +0700

    first commit in week 6

---
+++ week6/movie-database/src/App.js
@@ -0,0 +1,18 @@
+// Import Halaman Home
+import Home from "./pages/Home";
+
+function App() {
+  /**
+   * Menampilkan Halaman Home.
+   * Tag div bisa diganti dengan tag <>.
+   * Tag <> adalah React fragment
+   */
+  return (
+    <>
+      {/* <h2>Movie Database</h2> */}
+      <Home />
+    </>
+  );
+}
+
+export default App;
